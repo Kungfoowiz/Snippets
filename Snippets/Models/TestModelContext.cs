a53961e4a39237d176c1ca87f8e2452bb36a68ab
Edward L. Davies <kungfoowiz@hotmail.com>
2018-06-19 07:46:21 +0100

    Added entity framework example.

---
+++ Snippets/Models/TestModelContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Snippets.Models
+{
+
+    public class TestModelContext : DbContext
+    {
+        public DbSet<TestModel> TestModels { get; set; }
+    }
+
+}
