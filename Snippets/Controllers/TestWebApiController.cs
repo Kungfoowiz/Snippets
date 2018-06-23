3ef51e257403ea605924581b5bc8ff4d258d255e
Edward L. Davies <kungfoowiz@hotmail.com>
2018-06-23 09:14:54 +0100

    Added more webapi examples.

--- Snippets/Controllers/TestWebApiController.cs
+++ Snippets/Controllers/TestWebApiController.cs
@@ -15,7 +15,10 @@ namespace Snippets.Controllers
         // GET: api/TestWebApi
         // public IEnumerable<string> Get()
         // public IEnumerable<TestModel> Get()
-        public List<string> Get()
+        // public List<string> Get()
+        [HttpGet] // Can use custom method name then.
+        // public HttpResponseMessage Get()
+        public HttpResponseMessage Get(string search = "")
         {
             // return new string[] { "value1", "value2" };
 
@@ -27,13 +30,34 @@ namespace Snippets.Controllers
             //return models;
 
 
-            return data;
+            // return data;
+
+
+            HttpResponseMessage result;
+
+            var db = new TestModelContext();
+
+            var models = search == "" || search == null ?
+                db.TestModels.ToList() : db.TestModels.Where(x => x.Name.StartsWith(search)).ToList();
+
+            if (models != null)
+            {
+                result = Request.CreateResponse(HttpStatusCode.OK, models);
+            }
+            else
+            {
+                result = Request.CreateErrorResponse(HttpStatusCode.BadRequest, "No TestModels were found.");
+            }
+
+            return result;
         }
 
         // GET: api/TestWebApi/4
         // public string Get(int id)
         // public TestModel Get(int id)
-        public string Get(int id)
+        // public string Get(int id)
+        [HttpGet] // Can use custom method name then.
+        public HttpResponseMessage Get(int id)
         {
             // return "value";
 
@@ -45,28 +69,145 @@ namespace Snippets.Controllers
             //return model;
 
 
-            return data[id];
+            // return data[id];
+
+
+            HttpResponseMessage result;
+
+            var db = new TestModelContext();
+
+            var model = db.TestModels.Where(x => x.Id == id).FirstOrDefault();
+
+            if( model != null)
+            {
+                result = Request.CreateResponse(HttpStatusCode.OK, model);
+            }
+            else
+            {
+                result = Request.CreateErrorResponse(HttpStatusCode.NotFound, "TestModel id = [" + id.ToString() + "] was not found.");
+            }
+
+            return result;
         }
 
         // POST: api/TestWebApi
-        public void Post([FromBody]string value)
+        // public void Post([FromBody]string value)
+        // public void Post([FromBody] TestModel testModel)
+        [HttpPost] // Can use custom method name then.
+        public HttpResponseMessage Post([FromBody] TestModel testModel)
         {
-            data.Add(value);
+            // data.Add(value);
+
+
+            HttpResponseMessage result;
+
+            try
+            {
+                var db = new TestModelContext();
+
+                db.TestModels.Add(testModel);
+                db.SaveChanges();
+
+                result = Request.CreateResponse(HttpStatusCode.Created, testModel);
+                result.Headers.Location = new Uri(Request.RequestUri + "/" + testModel.Id.ToString());
+            }
+            catch(Exception exception)
+            {
+                result = Request.CreateErrorResponse(HttpStatusCode.BadRequest, exception);
+            }
+
+            return result;
         }
 
         // PUT: api/TestWebApi/4
-        public void Put(int id, [FromBody]string value)
+        // public void Put(int id, [FromBody]string value)
+        // public void Put(int id, [FromBody] TestModel testModel)
+        [HttpPut] // Can use custom method name then.
+        public HttpResponseMessage Put(int id, [FromBody] TestModel testModel)
         {
-            data[id] = value;
+            // data[id] = value;
+
+
+            //var db = new TestModelContext();
+
+            //var oldTestModel = db.TestModels.Where(x => x.Id == id).FirstOrDefault();
+
+            //oldTestModel.Name = testModel.Name;
+            //oldTestModel.Location = testModel.Location;
+
+            //db.SaveChanges();
+
+
+            HttpResponseMessage result;
+
+            var db = new TestModelContext();
+
+            var updateTestModel = db.TestModels.Where(x => x.Id == id).FirstOrDefault();
+
+            if (updateTestModel != null)
+            {
+                updateTestModel.Name = testModel.Name;
+                updateTestModel.Location = testModel.Location;
+
+                db.SaveChanges();
+
+                result = Request.CreateResponse(HttpStatusCode.OK, updateTestModel);
+                result.Headers.Location = new Uri(Request.RequestUri.ToString());
+            }
+            else
+            {
+                result = Request.CreateErrorResponse(HttpStatusCode.NotFound, "TestModel id = [" + id.ToString() + "] was not found.");
+            }
+
+            return result;
         }
 
         // DELETE: api/TestWebApi/4
-        public void Delete(int id)
+        // public void Delete(int id)
+        [HttpDelete] // Can use custom method name then.
+        public HttpResponseMessage Delete(int id)
         {
-            data.RemoveAt(id);
+            // data.RemoveAt(id);
+
+
+            //var db = new TestModelContext();
+
+            //var oldTestModel = db.TestModels.Where(x => x.Id == id).FirstOrDefault();
+
+            //db.TestModels.Remove(oldTestModel);
+            //db.SaveChanges();
+
+
+            HttpResponseMessage result;
+
+            try
+            {
+                var db = new TestModelContext();
+
+                var oldTestModel = db.TestModels.Where(x => x.Id == id).FirstOrDefault();
+
+                if (oldTestModel != null)
+                {
+                    db.TestModels.Remove(oldTestModel);
+                    db.SaveChanges();
+
+                    result = Request.CreateResponse(HttpStatusCode.OK);
+                }
+                else
+                {
+                    result = Request.CreateErrorResponse(HttpStatusCode.NotFound, "TestModel id = [" + id.ToString() + "] was not found.");
+                }
+            }
+            catch(Exception exception)
+            {
+                result = Request.CreateErrorResponse(HttpStatusCode.BadRequest, exception);
+            }
+
+            return result;
         }
 
         // PATCH: api/TestWebApi/4
+        [HttpPatch] // Can use custom method name then.
         public void Patch(int id)
         {
         }
