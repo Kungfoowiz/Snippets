22d060b3c87a563da76af0a7659f6ee4e9ca1144
Edward L. Davies <kungfoowiz@hotmail.com>
2018-06-24 08:53:35 +0100

    Added FromUri decorator examples; Added jquery ajax example to query TestWebApi webapi.

---
+++ Snippets/Controllers/TestWebApi2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Snippets.Controllers
+{
+    public class TestWebApi2Controller : Controller
+    {
+        // GET: TestWebApi2
+        public ActionResult Index()
+        {
+            return View("TestModels");
+        }
+    }
+}
